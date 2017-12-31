81551e6e39f08c507651539da9b6793e13dc0254
detfladder <alexiswong10@gmail.com>
2017-12-31 05:38:01 -0300

    Perfectioning Eva

--- Leviathan/Fortune Tellers/eva.py
+++ Leviathan/Fortune Tellers/eva.py
@@ -5,9 +5,12 @@ import pandas as pd
 
 import plotly as py
 
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
 import plotly.graph_objs as go
 
-Location = r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Fortune Tellers\Datasets\Eva knowledge\BTCCandlestickdata.csv'
+Location = r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Fortune Tellers\Datasets\Eva knowledge\btc-usd.csv'
 df = pd.read_csv(Location)
 
 
@@ -17,6 +20,25 @@ trace = go.Candlestick(x=df.index,
                        low=df.low,
                        close=df.close)
 data = [trace]
-py.offline.plot(data, filename='simple_candlestick.html')
+#py.offline.plot(data, filename='simple_candlestick.html')
 
 print(df)
+
+#Building the NN
+
+print('Building NN')
+
+model = Sequential()
+model.add(Dense(32, input_dim=5))
+model.add(Activation('relu'))
+model.add(Activation('softmax'))
+model.add(Dense(1))
+
+
+print ('NN Build')
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+print ('NN Compiled')
