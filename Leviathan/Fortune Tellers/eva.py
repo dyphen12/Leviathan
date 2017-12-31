c3d4979574692041d250e77afadbb899d73ebd01
detfladder <alexiswong10@gmail.com>
2017-12-31 18:27:24 -0300

    Setting the model dataset
    Adapting the dataset to the model, progress: NONE

--- Leviathan/Fortune Tellers/eva.py
+++ Leviathan/Fortune Tellers/eva.py
@@ -2,12 +2,13 @@
 
 
 import pandas as pd
-
+import numpy as np
 import plotly as py
+from sklearn.preprocessing import MinMaxScaler
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-
+from keras.layers.recurrent import LSTM
 import plotly.graph_objs as go
 
 Location = r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Fortune Tellers\Datasets\Eva knowledge\btc-usd.csv'
@@ -24,21 +25,34 @@ data = [trace]
 
 print(df)
 
+
+#Setting the dataset
+
+train_labels=np.array(df.open)
+train_samples=np.array([df.high,df.low,df.close])
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_train_samples= scaler.fit_transform((train_samples))
+
+
+
 #Building the NN
 
 print('Building NN')
 
 model = Sequential()
-model.add(Dense(32, input_dim=5))
+model.add(Dense(32, input_shape=train_samples.shape[1:]))
+
 model.add(Activation('relu'))
 model.add(Activation('softmax'))
 model.add(Dense(1))
 
-
 print ('NN Build')
 
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
+model.compile(optimizer='SGD',
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
 print ('NN Compiled')
+
+model.fit(scaled_train_samples,train_labels, batch_size=10,epochs=200,shuffle=True)
