c496792d5671edf557e32b5e48dec1efa74869af
detfladder <alexiswong10@gmail.com>
2017-12-31 04:52:56 -0300

    Meeting the eva03
    Welcome eva03!

--- Leviathan/Fortune Tellers/eva.py
+++ Leviathan/Fortune Tellers/eva.py
@@ -1,4 +1,22 @@
 #Eva the recurrent fortune teller of the btc-usd change
 
 
+import pandas as pd
 
+import plotly as py
+
+import plotly.graph_objs as go
+
+Location = r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Fortune Tellers\Datasets\Eva knowledge\BTCCandlestickdata.csv'
+df = pd.read_csv(Location)
+
+
+trace = go.Candlestick(x=df.index,
+                       open=df.open,
+                       high=df.high,
+                       low=df.low,
+                       close=df.close)
+data = [trace]
+py.offline.plot(data, filename='simple_candlestick.html')
+
+print(df)
---
+++ Leviathan/Fortune Tellers/eva03.py
@@ -0,0 +1,125 @@
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import model_from_json
+from keras.models import load_model
+
+from keras.layers import Dropout, Activation
+from keras.optimizers import SGD
+from keras.layers.recurrent import LSTM
+from keras.layers.convolutional import Convolution2D
+
+print ('Recurrent Neural Network: "EVA_03"')
+
+print('Loading the dataset.....')
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset
+dataframe = read_csv(r'C:\Users\Usuario\Documents\Eva\BTCanualdata.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+print('Dataset Loaded')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+print('Dataset normalized')
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print('Dataset splited into train set and test set')
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+# Setting the model
+
+print ('Building NN...')
+
+model = Sequential()
+model.add(LSTM(4, input_shape=(look_back, 1)))
+model.add(Dense(1))
+
+print ('NN Build')
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+print ('NN Compiled')
+
+print ('Training the NN...')
+
+model = load_model(r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Fortune Tellers\Models\Eva.h5')
+
+#model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+print ('NN Trained')
+
+print('Saving model')
+
+# Saving options
+
+#model.save('C:\Prisma Solutions\Programming\Eva\Eva.h5')  # creates a HDF5 file 'my_model.h5'
+#del model
+model = load_model(r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Fortune Tellers\Models\Eva.h5')
+# make predictions
+
+print ('Making Predictions')
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+print ('Preparing Plotting')
+
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+print ('Ready to plot')
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+
+
+
+
