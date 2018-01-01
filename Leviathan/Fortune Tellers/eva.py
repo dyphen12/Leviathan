138d7dbfc9a42c30fece49c476445840f7277e7a
detfladder <alexiswong10@gmail.com>
2017-12-31 23:16:13 -0300

    modeling eva
    shit

--- Leviathan/Fortune Tellers/eva.py
+++ Leviathan/Fortune Tellers/eva.py
@@ -8,6 +8,14 @@ from sklearn.preprocessing import MinMaxScaler
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.layers import Permute
+
+from keras.layers import RNN
+from keras.layers import SimpleRNNCell
+from keras.metrics import binary_crossentropy
+from keras.optimizers import SGD
+from keras.activations import softmax
+from keras.activations import relu
 from keras.layers.recurrent import LSTM
 import plotly.graph_objs as go
 
@@ -31,8 +39,36 @@ print(df)
 train_labels=np.array([df.Date])
 train_samples=np.array([df.open,df.high,df.low,df.close])
 
+dataset=train_samples
+
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_train_samples= scaler.fit_transform((train_samples))
+scaled_train_samples = scaler.fit_transform(train_samples)
+
+dataset = scaler.fit_transform(dataset)
+
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
+
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+
+
+trainX = np.reshape(train_samples, train_labels, train_samples)
+trainX = np.reshape(train_samples, train_labels, train_samples)
 
 
 
@@ -40,12 +76,12 @@ scaled_train_samples= scaler.fit_transform((train_samples))
 
 print('Building NN')
 
+print('cell build')
 model = Sequential()
-model.add(Dense(32, input_shape=train_samples.shape[1:]))
-
-model.add(Activation('relu'))
+model.add(LSTM())
 model.add(Activation('softmax'))
-model.add(Dense(1))
+model.add(Dense(1708))
+
 
 print ('NN Build')
 
