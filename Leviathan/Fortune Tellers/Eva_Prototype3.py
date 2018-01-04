f3d6d8619b2572f8b20b6358df2b97a78e994e2f
detfladder <alexiswong10@gmail.com>
2018-01-04 04:23:22 -0430

    Update
    The date reading problem is solved, now whe are turning the actual dataset into a correlation matrix

--- Leviathan/Fortune Tellers/Eva_Prototype3.py
+++ Leviathan/Fortune Tellers/Eva_Prototype3.py
@@ -17,12 +17,15 @@ from sklearn.metrics import mean_squared_error
 # fix random seed for reproducibility
 np.random.seed(7)
 
-print("EVA_Prototype2")
+print("EVA_Prototype3")
 print("")
 print("Initializing...")
 print("")
-print("EVA_Prototype2 // ON")
+print("EVA_Prototype3 // ON")
+print("")
 
+# DATASETTING PROCESS
+print("EVA_Prototype3 // DATASETTING PROCESS STARTED")
 #Analyze of the dataset
 print("Getting dataset // NOW USING: pandas")
 
@@ -34,7 +37,27 @@ dataset = dataset.astype('float32')
 
 print("Dataset loaded!")
 
+'Note: The dataset needs to be plotted by matplotlib, if is plotted then works.'
+
 import matplotlib.pyplot as plt
 
+print("Plotting dataset")
 plt.plot(dataset)
 plt.show()
+print("Dataset plotted")
+
+# normalize the dataset
+print("Normalizing dataset...")
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+
+#Setting the train a set sizes
+train_size = int(len(dataset) * 0.67) #splitting the set into train set with 67% of the observation
+test_size = len(dataset) - train_size #Splitting the set into test set with 33% of the observation (sustracting trainsize)
+
+#Splitting the data
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]#Ordering
+print(len(train), len(test))
+
