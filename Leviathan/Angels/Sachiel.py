b83807404a45d97709fb88616d2920ae157d81fc
detfladder <alexiswong10@gmail.com>
2018-01-06 03:11:27 -0430

    Sachiel update
    the module is almost done...

--- Leviathan/Angels/Sachiel.py
+++ Leviathan/Angels/Sachiel.py
@@ -155,6 +155,7 @@ while option != 0:
 
 	import pandas as pd
 	import matplotlib.pyplot as plt
+	from sklearn.preprocessing import MinMaxScaler
 
 	Location = r'C:\Users\Usuario\Documents\GitHub\Leviathan\Leviathan\Angels\BTCUSDtoEVA.csv'
 	dataframe = pd.read_csv(Location, usecols=[1], engine='python')
@@ -166,6 +167,51 @@ while option != 0:
 	plt.plot(dataset)
 	#plt.show()
 	print("Dataset plotted")
+
+	# normalize the dataset
+	print("Normalizing dataset...")
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(dataset)
+
+	# split into train and test sets
+
+	# Setting the train a set sizes
+	train_size = int(len(dataset) * 0.67)  # splitting the set into train set with 67% of the observation
+	test_size = len(
+		dataset) - train_size  # Splitting the set into test set with 33% of the observation (sustracting trainsize)
+
+	# Splitting the data
+	train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]  # Ordering
+	print(len(train), len(test))
+
+	print("Turning the dataset into a Correlation Matrix")
+
+
+	# I don't understand this shit----------------------
+
+	# function that convert an array of values into a dataset matrix (correlation matrix) aka "Reshape to x=t y=t+1"
+	def create_dataset(dataset, look_back=1):
+		dataX, dataY = [], []
+		for i in range(len(dataset) - look_back - 1):  # I guess this prints the Xrow(t) and the Yrow(t+1)
+			a = dataset[i:(i + look_back), 0]
+			dataX.append(a)  # Xrow (t)
+			dataY.append(dataset[i + look_back, 0])  # Yrow (t+1)
+		return np.array(dataX), np.array(dataY)  # Returns your shit ready
+
+
+	# Until here.---------------------------------------
+
+	# reshape into X=t and Y=t+1
+	look_back = 1
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+	print("Correlation Matrix Created")
+
+	print("")
+	print("Reshaping input data form for LSTM NN")
+	# reshape input to be [samples, time steps, features]
+	trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+	testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
 
 
